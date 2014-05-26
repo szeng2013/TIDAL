bae96b0dd21539f589730fc0c9d51a1944293991
Grant <gsheld@gmail.com>
2014-05-26 16:57:32 -0500

    Added arduino logic - gradient.ino

---
+++ gradient.ino.c
@@ -0,0 +1,44 @@
+#include "Wire.h"
+#include "BlinkM_funcs.h"
+
+byte addresses[7];
+const byte master_blinkm_1[3] = {255, 0, 0}; // red
+const byte master_blinkm_2[3] = {0, 0, 255}; // blue
+
+void setup()
+{
+	addresses = {-1, -1, -1, -1, -1, -1, -1};
+	BlinkM_beginWithPower();
+	for (byte i = 00x1; i < 00x8; i++) {
+		BlinkM_stopScript(i);  // turn off startup script
+	}
+}
+
+void loop()
+{
+	int number_found = get_all_blinkms(addresses);
+	for (int i=0, i < 7; i++) {
+		if (addresses[i] != -1) { // If address is valid
+			if (number_found > 1) {
+				BlinkM_setRGB(addresses[i], master_blinkm_1[0], master_blinkm_1[1], master_blinkm_1[2]);
+				number_found--;
+			} else {
+				BlinkM_setRGB(addresses[i], master_blinkm_2[0], master_blinkm_2[1], master_blinkm_2[2]);
+			}
+		}
+	}
+}
+
+int get_all_blinkms(byte (&addresses)[7]) // Pass array of addresses by value
+{
+	int count = 0;
+	for (int i = 1; i < 8; i++) {
+		if (BlinkM_receiveBytes(i, null, 1) == 0) { // Found a blinkm with that address.
+			addresses[i] = (byte) i;
+			count++;
+		} else {
+			addresses[i] = -1;
+		}
+	}
+	return count;
+}
